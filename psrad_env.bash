564d0911ba256f4ce8bfbec0340fc78c287eebc7
lkluft <lkluft@0c488439-3f28-0410-89a5-82d5fbb9eead>
2017-03-13 14:42:04 +0000

    * psrad_env.bash: Environment settings to compile and run PSRAD.
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/internal/users/lkluft/conrad/trunk@10018 0c488439-3f28-0410-89a5-82d5fbb9eead

---
+++ psrad_env.bash
@@ -0,0 +1,14 @@
+# Installation path for psrad
+export PSRAD_PATH='/scratch/uni/u237/users/lkluft/psrad'
+
+# Environment settings
+export F90="gfortran"  # FORTRAN compiler
+export HDF5ROOT="/sw/squeeze-x64/hdf5-1.8.8"
+export NETCDFROOT="/sw/squeeze-x64/netcdf-4.1.3-gccsys"
+export NETCDFFROOT="/sw/squeeze-x64/netcdf-4.1.3-gccsys"
+export LD_LIBRARY_PATH="${HDF5ROOT}/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${NETCDFROOT}/lib:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PSRAD_PATH}"
+
+# Include installation path to PATH (needed for bash).
+export PATH="${PATH}:${PSRAD_PATH}"
