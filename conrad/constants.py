Fix calculation of moist adiabatic lapse rate.
* lapserate.py (MoistLapseRate.get):

Fix bug in calculation of moist adiabatic lapse rate. Use the water
vapor mixing ratio for saturated air instead of the current value.

* constants.py:

Add gas constant for water vapor. Make all values related to gas
constants consistent with each other.

* convection.py (HardAdjustment.stabilize):

Adapt to new signature of `MoistLapseRate.get()`.

* atmosphere.py: Add comment.

git-svn-id: https://arts.mi.uni-hamburg.de/svn/internal/users/lkluft/conrad/trunk@10404 0c488439-3f28-0410-89a5-82d5fbb9eead

conrad/constants.py
@@ -2,6 +2,7 @@
 """Physical constants.
 """
 import scipy.constants as spc
+import typhon.constants as tyc
 
 
 # Phyiscal constants
@@ -9,8 +10,9 @@ Cp = isobaric_mass_heat_capacity = 1003.5  # J kg^-1 K^-1
 g = earth_standard_gravity = spc.g  # m s^-2
 stefan_boltzmann = 5.67e-8  # W m^-2 K^-4
 heat_of_vaporization = Lv = 2501000  # J k^-1
-specific_gas_constant_dry_air = R = 287  # J kg^-1 K^-1
-gas_constant_ratio = epsilon = 0.62197  # 1
+specific_gas_constant_dry_air = Rd = tyc.gas_constant_dry_air  # J kg^-1 K^-1
+specific_gas_constant_water_vapor = Rv = tyc.gas_constant_water_vapor  # J kg^-1 K^-1
+gas_constant_ratio = epsilon = Rd / Rv  # 1
 
 # Variable descriptions
 variable_description = {

conrad/lapserate.py
@@ -4,6 +4,7 @@ import abc
 import numbers
 
 import numpy as np
+from typhon.physics import e_eq_water_mk
 
 from conrad import constants
 
@@ -11,12 +12,12 @@ from conrad import constants
 class LapseRate(metaclass=abc.ABCMeta):
     """Base class for all lapse rate handlers."""
     @abc.abstractmethod
-    def get(self, T, VMR):
+    def get(self, T, p):
         """Return the atmospheric lapse rate.
 
         Parameters:
               T (ndarray): Atmospheric temperature [K].
-              VMR (ndarray): Water vapor content [VMR].
+              p (ndarray): Atmospheric pressure [Pa].
 
         Returns:
               ndarray: Temperature lapse rate [K/m].
@@ -25,20 +26,25 @@ class LapseRate(metaclass=abc.ABCMeta):
 
 class MoistLapseRate(LapseRate):
     """Moist adiabatic temperature lapse rate."""
-    def get(self, T, VMR):
-        # Use short forumula symbols for physical constants.
+    def get(self, T, p):
+        # Use short formula symbols for physical constants.
         g = constants.earth_standard_gravity
-        Lv = constants.heat_of_vaporization
-        R = constants.specific_gas_constant_dry_air
+        L = constants.heat_of_vaporization
+        Rd = constants.specific_gas_constant_dry_air
+        Rv = constants.specific_gas_constant_water_vapor
         epsilon = constants.gas_constant_ratio
         Cp = constants.isobaric_mass_heat_capacity
 
-        lapse = (
-            g * (1 + Lv * VMR / R / T)
-            / (Cp + Lv**2 * VMR * epsilon / R / T**2)
+        gamma_d = g / Cp  # dry lapse rate
+        q_saturated = epsilon * e_eq_water_mk(T) / p
+
+
+        gamma_m = (gamma_d * ((1 + (L * q_saturated) / (Rd * T)) /
+                              (1 + (L**2 * q_saturated) / (Cp * Rv * T**2))
+                              )
         )
 
-        return lapse
+        return gamma_m
 
 
 class FixedLapseRate(LapseRate):
@@ -51,7 +57,7 @@ class FixedLapseRate(LapseRate):
         """
         self.lapserate = lapserate
 
-    def get(self, T, VMR):
+    def get(self, T, p):
         if isinstance(self.lapserate, numbers.Number):
             return self.lapserate * np.ones(T.size)
         elif isinstance(self.lapserate, np.ndarray):