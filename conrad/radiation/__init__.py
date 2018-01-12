fadacae9c72145497c6f1a70dfc15007b70e5b94
sdacie <sdacie@0c488439-3f28-0410-89a5-82d5fbb9eead>
2018-01-12 09:49:43 +0000

    *radiation/__init__.py
    Added an RRTMG class, using the CliMT python wrapper.
    
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/internal/users/lkluft/conrad/trunk@10472 0c488439-3f28-0410-89a5-82d5fbb9eead

--- conrad/radiation/__init__.py
+++ conrad/radiation/__init__.py
@@ -10,7 +10,6 @@ import numpy as np
 from . import utils
 from conrad.utils import append_description
 
-
 logger = logging.getLogger()
 
 __all__ = [
@@ -172,3 +171,182 @@ class PSRAD(Radiation):
         append_description(ret)  # Append variable descriptions.
 
         return ret
+
+
+class RRTMG(Radiation):
+
+    def update_radiative_state(self, atmosphere, state0, sw=True):
+        
+        from sympl import DataArray 
+
+        plev = atmosphere['plev'].values
+        phlev = atmosphere['phlev'].values
+        numlevels = len(plev)
+        temperature = atmosphere.surface.temperature.data[0]
+        albedo = float(atmosphere.surface.albedo.data)
+        o2fraction = 0.21
+        zenith = self.zenith_angle
+        
+        state0['mid_levels'] = DataArray(np.arange(0, numlevels),
+              dims=('mid_levels'), attrs={'label': 'mid_levels'})
+        state0['interface_levels'] = DataArray(np.arange(0, numlevels+1),
+              dims=('interface_levels'), attrs={'label': 'interface_levels'})
+        state0['air_pressure'] = DataArray(plev,
+              dims=('mid_levels'), attrs={'units': 'Pa'})
+        state0['air_pressure_on_interface_levels'] = DataArray(phlev,
+              dims=('interface_levels'), attrs={'units': 'Pa'})
+        state0['air_temperature'] = DataArray(atmosphere['T'][0, :].data,
+              dims=('mid_levels'), attrs={'units': 'degK'})
+        vmr_h2o = atmosphere['H2O'][0, :].data
+        specific_humidity = vmr_h2o / (1 + vmr_h2o)
+        state0['specific_humidity'] = DataArray(specific_humidity,
+              dims=('mid_levels'), attrs={'units': 'g/g'})
+        state0['mole_fraction_of_oxygen_in_air'] = DataArray(
+                o2fraction*np.ones(numlevels,), dims=('mid_levels'),
+                attrs={'units': 'mole/mole'})
+        # Set trace gas concentrations
+        for gasname, gas in [('mole_fraction_of_methane_in_air', 'CH4'),
+                             ('mole_fraction_of_carbon_dioxide_in_air', 'CO2'),
+                             ('mole_fraction_of_nitrous_oxide_in_air', 'N2O'),
+                             ('mole_fraction_of_ozone_in_air', 'O3')]:
+            state0[gasname] = DataArray(atmosphere[gas][0, :].data,
+                  dims=('mid_levels'), attrs={'units': 'mole/mole'})
+        for gasname in ['mole_fraction_of_carbon_tetrachloride_in_air',
+                        'mole_fraction_of_cfc22_in_air',
+                        'mole_fraction_of_cfc12_in_air',
+                        'mole_fraction_of_cfc11_in_air']:
+            state0[gasname] = DataArray(np.zeros(numlevels,), dims=('mid_levels'),
+                  attrs={'units': 'mole/mole'})
+        
+        # Set cloud quantities to zero.
+        for quant in ['cloud_ice_particle_size',
+                      'mass_content_of_cloud_liquid_water_in_atmosphere_layer',
+                      'mass_content_of_cloud_ice_in_atmosphere_layer',
+                      'cloud_water_droplet_radius',
+                      'cloud_area_fraction_in_atmosphere_layer']:
+            units = state0[quant].units
+            state0[quant] = DataArray(np.zeros(numlevels,), dims=('mid_levels'),
+                  attrs={'units': units})
+        
+        if not sw: # Longwave specific changes
+            num_lw_bands = len(state0['num_longwave_bands'])
+            state0['longwave_optical_thickness_due_to_cloud'] = DataArray(
+                    np.zeros((1, num_lw_bands, 1, numlevels)),
+                    dims=('longitude', 'num_longwave_bands', 'latitude', 'mid_levels'),
+                    attrs={'units': 'dimensionless'})
+            state0['longwave_optical_thickness_due_to_aerosol'] = DataArray(
+                    np.zeros((1, 1, numlevels, num_lw_bands)),
+                    dims=('longitude', 'latitude', 'mid_levels', 'num_longwave_bands'),
+                    attrs={'units': 'dimensionless'})
+        if sw: # Shortwave specific changes
+            num_sw_bands = len(state0['num_shortwave_bands'])
+            for quant in ['cloud_forward_scattering_fraction', 'cloud_asymmetry_parameter',
+                          'shortwave_optical_thickness_due_to_cloud',
+                          'single_scattering_albedo_due_to_cloud']:
+                state0[quant] = DataArray(np.zeros((1, num_sw_bands, 1, numlevels)),
+                         dims=('longitude', 'num_shortwave_bands', 'latitude',
+                               'mid_levels'),
+                         attrs={'units': 'dimensionless'})
+            num_aerosols = len(state0['num_ecmwf_aerosols'])
+            state0['aerosol_optical_depth_at_55_micron'] = DataArray(
+                    np.zeros((1, 1, numlevels, num_aerosols)),
+                    dims=('longitude', 'latitude', 'mid_levels', 'num_ecmwf_aerosols'),
+                    attrs={'units': 'dimensionless'})
+            for quant in ['shortwave_optical_thickness_due_to_aerosol',
+                          'single_scattering_albedo_due_to_aerosol',
+                          'aerosol_asymmetry_parameter']:
+                state0[quant] = DataArray(np.zeros((1, 1, numlevels, num_sw_bands)),
+                         dims=('longitude', 'latitude', 'mid_levels', 'num_shortwave_bands'),
+                         attrs={'units': 'dimensionless'})
+        #TODO: should all the aerosol values be zero?!
+        
+        # Surface quantities
+        state0['surface_temperature'] = DataArray(np.array([[temperature]]),
+              dims={'longitude', 'latitude'},
+              attrs={'units': 'degK'})
+        if sw: # surface properties required only for shortwave
+            for surface_albedo in ['surface_albedo_for_diffuse_near_infrared',
+                           'surface_albedo_for_direct_near_infrared',
+                           'surface_albedo_for_diffuse_shortwave',
+                           'surface_albedo_for_direct_shortwave']:
+                state0[surface_albedo] = DataArray(np.array([[albedo]]), 
+                         dims={'longitude', 'latitude'}, attrs={'units': 'dimensionless'})
+            # Sun
+            state0['zenith_angle'] = DataArray(np.array([[zenith]]),
+                     dims={'longitude', 'latitude'}, attrs={'units': 'radians'})
+        
+        return state0
+        
+    def radiative_fluxes(self, atmosphere):
+        
+        # TODO: Do all of this only once (first iteration),
+        # and then just update the radiative state.
+        import climt
+        rad_lw = climt.RRTMGLongwave()
+        rad_sw = climt.RRTMGShortwave()
+        state0_lw = climt.get_default_state([rad_lw])
+        state0_sw = climt.get_default_state([rad_sw])
+        
+        self.update_radiative_state(atmosphere, state0_lw, sw=False)
+        self.update_radiative_state(atmosphere, state0_sw, sw=True)
+        
+        lw_fluxes = rad_lw(state0_lw)
+        sw_fluxes = rad_sw(state0_sw)
+        
+        return lw_fluxes, sw_fluxes
+    
+    def get_heatingrates(self, atmosphere):
+        
+        lw_dT_fluxes, sw_dT_fluxes = self.radiative_fluxes(atmosphere)
+        lw_fluxes = lw_dT_fluxes[1]
+        sw_fluxes = sw_dT_fluxes[1]
+        
+        ret = Dataset({
+            # General atmospheric properties.
+            'z': atmosphere['z'],
+            # Longwave fluxes and heatingrates.
+            'lw_htngrt': (['time', 'plev'],
+                          lw_fluxes['longwave_heating_rate'][0].data),
+            'lw_htngrt_clr': (['time', 'plev'],
+                              lw_fluxes['longwave_heating_rate_assuming_clear_sky'][0].data),
+            'lw_flxu': (['time', 'phlev'],
+                        lw_fluxes['upwelling_longwave_flux_in_air'][0].data),
+            'lw_flxd': (['time', 'phlev'],
+                        lw_fluxes['downwelling_longwave_flux_in_air'][0].data),
+            'lw_flxu_clr': (['time', 'phlev'],
+                            lw_fluxes['upwelling_longwave_flux_in_air_assuming_clear_sky'][0].data),
+            'lw_flxd_clr': (['time', 'phlev'],
+                            lw_fluxes['downwelling_longwave_flux_in_air_assuming_clear_sky'][0].data),
+            # Shortwave fluxes and heatingrates.
+            'sw_htngrt': (['time', 'plev'],
+                          sw_fluxes['shortwave_heating_rate'][0].data),
+            'sw_htngrt_clr': (['time', 'plev'],
+                              sw_fluxes['shortwave_heating_rate_assuming_clear_sky'][0].data),
+            'sw_flxu': (['time', 'phlev'], sw_fluxes['upwelling_shortwave_flux_in_air'][0].data),
+            'sw_flxd': (['time', 'phlev'], sw_fluxes['downwelling_shortwave_flux_in_air'][0].data),
+            'sw_flxu_clr': (['time', 'phlev'],
+                            sw_fluxes['upwelling_shortwave_flux_in_air_assuming_clear_sky'][0].data),
+            'sw_flxd_clr': (['time', 'phlev'],
+                            sw_fluxes['downwelling_shortwave_flux_in_air_assuming_clear_sky'][0].data),
+            # Net heatingrate.
+            'net_htngrt': (['time', 'plev'],
+                           lw_fluxes['longwave_heating_rate'][0].data
+                           + sw_fluxes['shortwave_heating_rate'][0].data),
+            # Radiation budget at top of the atmosphere (TOA).
+            'toa': (['time'],
+                np.array([sw_fluxes['downwelling_shortwave_flux_in_air'][0, 0, -1].data
+                - sw_fluxes['upwelling_shortwave_flux_in_air'][0, 0, -1].data
+                + lw_fluxes['downwelling_longwave_flux_in_air'][0, 0, -1].data
+                - lw_fluxes['upwelling_longwave_flux_in_air'][0, 0, -1].data])),
+            },
+            coords={
+                'time': [0],
+                'phlev': atmosphere['phlev'].values,
+                'plev': atmosphere['plev'].values,
+            }
+            )
+
+        append_description(ret)  # Append variable descriptions.
+
+        return ret
+    
