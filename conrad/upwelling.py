84305b355c22a2e234ad14639a6850ff7e72eee9
sdacie <sdacie@0c488439-3f28-0410-89a5-82d5fbb9eead>
2018-02-01 13:03:11 +0000

    *upwelling.py
    Corrected the upwelling so that it is only applied above the convective top or a specified level 'lowest_level'. 
    Changed the units of the velocity, so that velocities must be specified in mm/s.
    
    
    
    git-svn-id: https://arts.mi.uni-hamburg.de/svn/internal/users/lkluft/conrad/trunk@10488 0c488439-3f28-0410-89a5-82d5fbb9eead

--- conrad/upwelling.py
+++ conrad/upwelling.py
@@ -25,35 +25,39 @@ class NoUpwelling(Upwelling):
 
 class StratosphericUpwelling(Upwelling):
 
-    def __init__(self, w=43.2):
+    def __init__(self, w=0.2, lowest_level=None):
         """Create a upwelling handler.
 
         Parameters:
-            w (float): Upwelling velocity in m/day.
-                43.2 is equilvalent to 0.5 mm/s.
+            w (float): Upwelling velocity in mm/s.
+            lowest_level (int or None): The index of the lowest level to which
+                the upwelling is applied. If none, uses the top of convection.
         """
-        self.w = w
+        self.w = w * 86.4 # in m/day
+        self.lowest_level = lowest_level
 
     def cool(self, atmosphere, radheat, timestep):
         """Apply cooling above the convective top (level where the net
         radiative heating becomes small)."""
 
-        # arbitrary value close to 0, not too close to allow the upwelling to
-        # occur when the upper atmosphere is not yet in radiative equilibrium.
-        radheatmin = 0.0001
-
+        # profile if the whole of the atmosphere is cooled
         Q = self.coolingrates(atmosphere)
-        T = atmosphere['T'][0, :]
-        T_new = T + Q * timestep
+        T = atmosphere['T'][0, :].data
 
-        try:
-            contopi = np.min(np.where(radheat < radheatmin))
-        except ValueError:
-            # If a ValueError is thrown, no minimum in radiative heating has
-            # been found. Return the function without applying any upwelling.
-            return
-
-        atmosphere['T'].values[0, contopi:] = T_new[contopi:]
+        if self.lowest_level is not None:
+            contopi = self.lowest_level
+        else:
+            # arbitrary value close to 0, not too close to allow the upwelling to
+            # occur when the upper atmosphere is not yet in radiative equilibrium.
+            radheatmin = 0.0001
+            try:
+                contopi = np.min(np.where(radheat[0, :] > -radheatmin))
+            except ValueError:
+                # If a ValueError is thrown, no minimum in radiative heating has
+                # been found. Return the function without applying any upwelling.
+                return
+        T_new = T[contopi:] + Q[contopi:] * timestep
+        atmosphere['T'][0, contopi:] = T_new
 
     def coolingrates(self, atmosphere):
         """Get cooling rates associated with the upwelling velocity w."""
