8af8bc671b933bae289e6e828d5ff625f0f99456
SallyDa <sallydacie@gmail.com>
2018-06-15 16:55:59 +0200

    Changed the fixed stratospheric humidity case, so that a smooth transition occurs.

--- konrad/humidity.py
+++ konrad/humidity.py
@@ -14,8 +14,7 @@ logger = logging.getLogger(__name__)
 class Humidity(metaclass=abc.ABCMeta):
     """Base class to define abstract methods for all humidity handlers."""
     def __init__(self, rh_surface=0.8, rh_tropo=0.3, p_tropo=170e2,
-                 vmr_strato=None, transition_depth=None, vmr_profile=None,
-                 rh_profile=None):
+                 vmr_strato=None, vmr_profile=None, rh_profile=None):
         """Create a humidity handler.
 
         Parameters:
@@ -25,8 +24,6 @@ class Humidity(metaclass=abc.ABCMeta):
                 in the upper-troposphere.
             p_tropo (float): Pressure level of second humidity maximum [Pa].
             vmr_strato (float): Stratospheric water vapor VMR.
-            transition_depth (float): Transition depth from humidity at the
-                cold point to the straotpsheric background [m].
             vmr_profile (ndarray): Water vapour volume mixing ratio profile.
             rh_profile (ndarray): Relative humidity profile.
                 Is ignored if the `vmr_profile` is given.
@@ -35,7 +32,6 @@ class Humidity(metaclass=abc.ABCMeta):
         self.rh_tropo = rh_tropo
         self.p_tropo = p_tropo
         self.vmr_strato = vmr_strato
-        self.transition_depth = transition_depth
 
         self.vmr_profile = vmr_profile
         self.rh_profile = rh_profile
@@ -124,30 +120,13 @@ class Humidity(metaclass=abc.ABCMeta):
         # the top of the atmosphere.
         cp_index = int(np.argmin(T[p > cold_point_min]))
 
-        # Keep water vapor VMR values above the cold point tropopause constant.
+        # Keep stratospheric water vapor VMR values constant.
         # If stratospheric background = None, use cold point VMR, otherwise
         # transition to the stratospheric background value
         if self.vmr_strato is None:
             vmr[cp_index:] = vmr[cp_index]
         else:
-            d = self.transition_depth
-            if d is None:
-                raise ValueError('Specify a transition depth for the ' +
-                                 'stratospheric water vapour mixing ratio.')
-            vmr_strato = self.vmr_strato
-            vmr_cp = vmr[cp_index]
-            cpz = z[cp_index]  # height of the base of the transition
-
-            # index of the top of the transition, this breaks if d is too large
-            # and z is nowhere bigger than `cpz + transition_depth`.
-            d_i = np.min(np.where(z > cpz+self.transition_depth))
-
-            vmr[cp_index:d_i] = (
-                -np.cos(np.pi*(z[cp_index:d_i]-cpz)/self.transition_depth)
-                * (vmr_strato - vmr_cp)/2
-                + (vmr_strato + vmr_cp)/2
-            )
-            vmr[d_i:] = vmr_strato
+            vmr[np.argmax(vmr < self.vmr_strato):] = self.vmr_strato
 
         return vmr
 
