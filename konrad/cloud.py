a866863181a6c1daff8a06b17577649e1eb90251
SallyDa <sallydacie@gmail.com>
2018-09-19 15:58:05 +0200

    Added a low cloud class, where clouds are fixed with height (in the planetary boundary layer). Updated docstrings.

--- konrad/cloud.py
+++ konrad/cloud.py
@@ -50,10 +50,30 @@ class Cloud(metaclass=abc.ABCMeta):
                  lw_optical_thickness=0, sw_optical_thickness=0,
                  forward_scattering_fraction=0.8, asymmetry_parameter=0.85,
                  single_scattering_albedo=0.9):
-        """Create a cloud handler.
+        """Create a cloud.
 
         Parameters:
-            forward_scattering_fraction
+            numlevels (int): number of model levels
+            num_lw_bands (int): number of longwave bands, for RRTMG this is 16
+            num_sw_bands (int): number of shortwave bands, for RRTMG this is 14
+            mass_water (float / DataArray): mass content of cloud liquid water
+                [kg m-2]
+            mass_ice (float / DataArray): mass content of cloud ice [kg m-2]
+            cloud_fraction (float / DataArray): cloud area fraction
+            ice_particle_size (float / DataArray): cloud ice particle size
+                [micrometers]
+            droplet_radius (float / DataArray): cloud water droplet radius
+                [micrometers]
+            lw_optical_thickness (float / DataArray): longwave optical
+                thickness of the cloud
+            sw_optical_thickness (float / DataArray): shortwave optical
+                thickness of the cloud
+            forward_scattering_fraction (float / DataArray): cloud forward
+                scattering fraction (for the shortwave component of RRTMG)
+            asymmetry_parameter (float / DataArray): cloud asymmetry parameter
+                (for the shortwave component of RRTMG)
+            single_scattering_albedo (float / DataArray): single scattering
+                albedo due to cloud (for the shortwave component of RRTMG)
         """
         self.mass_content_of_cloud_liquid_water_in_atmosphere_layer = \
                 get_p_data_array(mass_water, numlevels=numlevels)
@@ -93,9 +113,19 @@ class Cloud(metaclass=abc.ABCMeta):
                 single_scattering_albedo, numlevels=numlevels,
                 numbands=num_sw_bands)
 
+    def calculate_mass_cloud(self, cloud_fraction_array, z, density):
+        dz = np.hstack((np.diff(z)[0], np.diff(z)))
+        mass_array = cloud_fraction_array * density * 10 ** -3 * dz
+        mass = DataArray(mass_array, dims=('mid_levels',),
+                         attrs={'units': 'kg m^-2'})
+        return mass
+
     @abc.abstractmethod
     def update_cloud_profile(self, atmosphere, **kwargs):
-        """Return the cloud parameters for the radiation scheme."""
+        """Return the cloud parameters for the radiation scheme.
+        Parameters:
+            atmosphere (konrad.atmosphere.Atmosphere): atmosphere model
+        """
 
 
 class ClearSky(Cloud):
@@ -104,7 +134,7 @@ class ClearSky(Cloud):
 
 
 class HighCloud(Cloud):
-    def __init__(self, z, cloud_top=12000, depth=500, area_fraction=1,
+    def __init__(self, z, cloud_top=12000, depth=500, cloud_fraction_in_cloud=1,
                  ice_density=0.5, lw_optical_thickness=10,
                  sw_optical_thickness=10,
                  forward_scattering_fraction=0.8, asymmetry_parameter=0.85,
@@ -114,21 +144,19 @@ class HighCloud(Cloud):
             z (ndarray): altitude values [m]
             cloud_top (int): Cloud top height [m]
             depth (int): Cloud depth / thickness of cloud [m]
-            area_fraction (int/float): Area fraction covered by cloud
-            ice_density (float): density of ice in the cloud [g m^-3]
-            lw_optical_thickness (float): optical thickness of the cloud in the
-                longwave [dimensionless]
-            sw_optical_thickness (float): optical thickness of the cloud in the
-                shortwave [dimensionless]
+            cloud_fraction_in_cloud (int): Cloud area fraction of the cloud for
+                the model levels between cloud_top-depth and cloud_top
+
+        Other parameters: as in the parent class.
         """
         cloud_fraction_array = np.zeros(z.shape)
         cloud_fraction_array[(z < cloud_top) & (
-                z > cloud_top-depth)] = area_fraction
+                z > cloud_top-depth)] = cloud_fraction_in_cloud
         cloud_fraction = DataArray(cloud_fraction_array, dims=('mid_levels',),
                                    attrs={'units': 'dimensionless'})
 
-        mass_ice = self.calculate_mass_cloud_ice(cloud_fraction_array, z,
-                                                 ice_density=ice_density)
+        mass_ice = self.calculate_mass_cloud(cloud_fraction_array, z,
+                                             ice_density)
 
         super().__init__(cloud_fraction=cloud_fraction, mass_ice=mass_ice,
              lw_optical_thickness=lw_optical_thickness,
@@ -141,16 +169,9 @@ class HighCloud(Cloud):
         self._norm_level = cloud_top
         self._f = None
 
-    def calculate_mass_cloud_ice(self, cloud_fraction_array, z, ice_density):
-        dz = np.hstack((np.diff(z)[0], np.diff(z)))
-        mass_ice_array = cloud_fraction_array * ice_density * 10 ** -3 * dz
-        mass_ice = DataArray(mass_ice_array, dims=('mid_levels',),
-                             attrs={'units': 'kg m^-2'})
-        return mass_ice
-
-
     def update_cloud_profile(self, atmosphere, **kwargs):
-
+        """ Keep the cloud attached to the convective top.
+        """
         if self._f is None:
             normed_height = atmosphere.get_values(
                 'z', keepdims=False)-self._norm_level
@@ -169,6 +190,65 @@ class HighCloud(Cloud):
             dims=('mid_levels',),
             attrs={'units': 'dimensionless'})
 
-        mass_ice = self.calculate_mass_cloud_ice(cloud_fraction_array, z,
-                                                 self._ice_density)
+        mass_ice = self.calculate_mass_cloud(cloud_fraction_array, z,
+                                             self._ice_density)
         self.mass_content_of_cloud_ice_in_atmosphere_layer = mass_ice
+
+
+class LowCloud(Cloud):
+    def __init__(self, z, cloud_top=2000, depth=1500, cloud_fraction_in_cloud=1,
+                 water_density=0.5, lw_optical_thickness=10,
+                 sw_optical_thickness=10,
+                 forward_scattering_fraction=0.8, asymmetry_parameter=0.85,
+                 single_scattering_albedo=0.9):
+        """
+        Parameters:
+            z (ndarray): altitude values [m]
+            cloud_top (int): Cloud top height [m]
+            depth (int): Cloud depth / thickness of cloud [m]
+            cloud_fraction_in_cloud (int): Cloud area fraction of the cloud for
+                the model levels between cloud_top-depth and cloud_top
+
+        Other parameters: as in the parent class.
+        """
+        cloud_fraction_array = np.zeros(z.shape)
+        cloud_fraction_array[(z < cloud_top) & (
+                z > cloud_top-depth)] = cloud_fraction_in_cloud
+        cloud_fraction = DataArray(cloud_fraction_array, dims=('mid_levels',),
+                                   attrs={'units': 'dimensionless'})
+
+        mass_water = self.calculate_mass_cloud(cloud_fraction_array, z,
+                                               water_density)
+
+        super().__init__(cloud_fraction=cloud_fraction, mass_water=mass_water,
+             lw_optical_thickness=lw_optical_thickness,
+             sw_optical_thickness=sw_optical_thickness,
+             forward_scattering_fraction=forward_scattering_fraction,
+             asymmetry_parameter=asymmetry_parameter,
+             single_scattering_albedo=single_scattering_albedo)
+
+        self._water_density = water_density
+        self._f = None
+
+    def update_cloud_profile(self, atmosphere, **kwargs):
+        """ Keep the cloud fixed with height, in the planetary boundary layer.
+        """
+        if self._f is None:
+            height = atmosphere.get_values(
+                'z', keepdims=False)
+            self._f = interp1d(
+                height,
+                self.cloud_area_fraction_in_atmosphere_layer.values,
+                fill_value='extrapolate',
+            )
+
+        z = atmosphere.get_values('z', keepdims=False)
+        cloud_fraction_array = self._f(z)
+        self.cloud_area_fraction_in_atmosphere_layer = DataArray(
+            cloud_fraction_array,
+            dims=('mid_levels',),
+            attrs={'units': 'dimensionless'})
+
+        mass = self.calculate_mass_cloud(cloud_fraction_array, z,
+                                         self._water_density)
+        self.mass_content_of_cloud_liquid_water_in_atmosphere_layer = mass
