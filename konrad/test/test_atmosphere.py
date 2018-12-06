402881adb9b6d7842742be0cde0ad2516ba39170
Lukas Kluft <lukas.kluft@gmail.com>
2018-12-06 11:11:51 +0100

    Add tests for temperature diagnostic methods.

--- konrad/test/test_atmosphere.py
+++ konrad/test/test_atmosphere.py
@@ -1,6 +1,14 @@
 import numpy as np
+import pytest
 
-from konrad import atmosphere
+from konrad import (atmosphere, utils)
+
+
+@pytest.fixture
+def atmosphere_obj():
+    plev, _ = utils.get_pressure_grids(1000e2, 1, 50)
+
+    return atmosphere.Atmosphere(plev=plev)
 
 
 class TestAtmosphere:
@@ -9,3 +17,19 @@ class TestAtmosphere:
         plev = np.array([1000e2, 750e2, 500e2, 100e2, 10e2, 1e2])
 
         atmosphere.Atmosphere(plev=plev)
+
+    def test_cold_point_index(self, atmosphere_obj):
+        """Test retrieval of the cold point index."""
+        assert atmosphere_obj.get_cold_point_index() == 11
+
+    def test_triple_point_index(self, atmosphere_obj):
+        """Test retrieval of the triple point index."""
+        assert atmosphere_obj.get_triple_point_index() == 2
+
+    def test_cold_point_plev(self, atmosphere_obj):
+        """Test retrieval of the cold point pressure."""
+        assert np.isclose(atmosphere_obj.get_cold_point_plev(), 20023.067)
+
+    def test_triple_point_plev(self, atmosphere_obj):
+        """Test retrieval of the triple point plev."""
+        assert np.isclose(atmosphere_obj.get_triple_point_plev(), 74287.948)
