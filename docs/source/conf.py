050b69c002472af2516af5b172ed19c3ad99a65c
Lukas Kluft <lukas.kluft@gmail.com>
2019-04-25 17:06:33 +0200

    Enable matplotlib plot directive

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'matplotlib.sphinxext.plot_directive',
 ]
 
 # -- General configuration ------------------------------------------------
