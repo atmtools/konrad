6a6d0b42c36c38c3c8598214485bc55f33e28d33
Lukas Kluft <lukas.kluft@gmail.com>
2021-10-27 11:35:02 +0200

    Drop Python 3.6 support and add Python 3.9

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3.6',
     include_package_data=True,
